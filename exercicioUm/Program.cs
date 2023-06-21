d082a64e28b54b8b3af94976167afff001d4c85b
Thauany Dias <98629101+thauanyddaf@users.noreply.github.com>
2023-06-21 20:43:21 -0300

    Add files via upload

---
+++ exercicioUm/Program.cs
@@ -0,0 +1,25 @@
+﻿    Console.WriteLine("Seja bem vindo ao jogo Número secreto! ");
+    Random numAleatorio = new Random();
+    int numSorteado = numAleatorio.Next(1, 100);
+
+
+do  {
+    Console.WriteLine("Adivinhe qual será o número sorteado de 1 a 100. Digite o seu palpite: ");
+    int numDigitado = int.Parse(Console.ReadLine());
+    
+
+    if(numDigitado == numSorteado)
+    {
+    Console.WriteLine("Parabéns! Você acertou o número.");
+    break;
+    } else if(numDigitado < numSorteado)
+    {
+    Console.WriteLine("Ops! O número é menor que o sorteado.");
+    } else
+    {
+    Console.WriteLine("Ops! O número é maior que o sorteado.");
+    }
+    } while(true);
+    
+    Console.WriteLine("O jogo acabou. Você acertou o número secreto!");
+
